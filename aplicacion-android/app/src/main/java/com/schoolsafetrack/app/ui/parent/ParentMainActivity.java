0c672a164bb1af57e22e17efa330073fba71059a
nbch0203 <nixonbcruzh@gmail.com>
2026-04-08 13:25:46 +0000

    fix: correct XML parse error in ic_chevron_right, fix back arrow icon, remove unused import

--- aplicacion-android/app/src/main/java/com/schoolsafetrack/app/ui/parent/ParentMainActivity.java
+++ aplicacion-android/app/src/main/java/com/schoolsafetrack/app/ui/parent/ParentMainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
